0df9a4cd0845b97e6e485f305acca5430da3a739
吉翔 <jx4554@gmail.com>
2017-11-27 17:08:46 +0800

    upadte #get_custom_fields and  delete #get_custom_field for v2.2

--- lib/insightly2/dsl/custom_fields.rb
+++ lib/insightly2/dsl/custom_fields.rb
@@ -7,16 +7,17 @@ module Insightly2
     # @param [String, Fixnum] id A CustomField's ID.
     # @raise [ArgumentError] If the method arguments are blank.
     # @return [Insightly2::Resources::CustomField, nil].
-    def get_custom_field(id: nil)
-      raise ArgumentError, "ID cannot be blank" if id.blank?
-      Resources::CustomField.parse(request(:get, "CustomFields/#{id}"))
-    end
+    # def get_custom_field(id: nil)
+    #   raise ArgumentError, "ID cannot be blank" if id.blank?
+    #   Resources::CustomField.parse(request(:get, "CustomFields/#{id}"))
+    # end
 
     # GET /v2.1/CustomFields
     # Get a list of custom fields.
     # @return [Array, nil].
-    def get_custom_fields
-      Resources::CustomField.parse(request(:get, "CustomFields"))
+    def get_custom_fields(query={})
+      url = Utils::UrlHelper.build_url(path: 'CustomFields', params: query)
+      Resources::CustomField.parse(request(:get, url))
     end
   end
 end
