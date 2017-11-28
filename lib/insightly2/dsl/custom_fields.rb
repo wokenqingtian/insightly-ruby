ab79628877cef61b6cbf538fcc4803ef56051f87
吉翔 <jx4554@gmail.com>
2017-11-28 11:16:32 +0800

    fix comment of #get_custom_fields for v2.2

--- lib/insightly2/dsl/custom_fields.rb
+++ lib/insightly2/dsl/custom_fields.rb
@@ -2,17 +2,8 @@ require 'insightly2/dsl'
 
 module Insightly2
   module DSL::CustomFields
-    # GET /v2.1/CustomFields/{id}
-    # Get a custom field.
-    # @param [String, Fixnum] id A CustomField's ID.
-    # @raise [ArgumentError] If the method arguments are blank.
-    # @return [Insightly2::Resources::CustomField, nil].
-    # def get_custom_field(id: nil)
-    #   raise ArgumentError, "ID cannot be blank" if id.blank?
-    #   Resources::CustomField.parse(request(:get, "CustomFields/#{id}"))
-    # end
 
-    # GET /v2.1/CustomFields
+    # GET /v2.2/CustomFields
     # Get a list of custom fields.
     # @return [Array, nil].
     def get_custom_fields(query={})
